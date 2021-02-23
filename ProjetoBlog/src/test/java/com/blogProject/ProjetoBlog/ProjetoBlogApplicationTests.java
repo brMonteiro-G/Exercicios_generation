033c48d0b573245fefb59ae00ea158bc641fb947
Gabriel Monteiro <brgabriel.monteiro@gmail.com>
2021-02-23 18:37:24 -0300

    Projeto blog completo

---
+++ ProjetoBlog/src/test/java/com/blogProject/ProjetoBlog/ProjetoBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogProject.ProjetoBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
